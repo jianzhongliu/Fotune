e216c87f64e0471b108fe1de8793c8793328473f
jianzhongliu <767381290@qq.com>
2013-08-15 22:59:03 +0800

    Initial Commit

---
+++ Fortune/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fortune
+//
+//  Created by yons on 13-8-15.
+//  Copyright (c) 2013年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
